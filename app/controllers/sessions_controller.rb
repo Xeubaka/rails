5e1ec428452eb32cd7f710e4624270dee8bf8934
Luis Roberto <lrs.juca@gmail.com>
2019-01-14 09:57:44 -0200

    ultimos detalhes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      redirect_to contacts_path
+    else
+      flash.now[:danger] = 'Email ou senha invalidos'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = 'Logout com sucesso'
+    redirect_to entrar_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :contacts
 
   root to: "static_page#index"
 
   get 'sobre', to: 'static_page#sobre'
-
   get 'contato', to: 'static_page#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
 
+  resources :contacts
+  resources :users, only: [:new, :create, :show, :edit, :update]
 
 end
